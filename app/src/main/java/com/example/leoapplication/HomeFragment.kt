0b30769584164c6d8bff223745ebdfa047906c95
fidanquliyeva <quliyevafidan70@gmail.com>
2025-08-23 19:58:00 +0400

    some functionality about cardanimation

--- app/src/main/java/com/example/leoapplication/HomeFragment.kt
+++ app/src/main/java/com/example/leoapplication/HomeFragment.kt
@@ -1,11 +1,19 @@
 package com.example.leoapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.isInvisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.AppBarLayout
 
 class HomeFragment : Fragment() {
@@ -20,13 +28,55 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val motionLayout = view.findViewById<MotionLayout>(R.id.motionLayout)
-        val appBarLayout = view.findViewById<AppBarLayout>(R.id.app_bar)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        appBarLayout.addOnOffsetChangedListener { _, verticalOffset ->
-            val totalScrollRange = appBarLayout.totalScrollRange
-            val progress = -verticalOffset / totalScrollRange.toFloat()
-            motionLayout.progress = progress
+        // Dummy data
+        val list = mutableListOf<String>()
+        for (i in 1..30) list.add("Item #$i")
+        recyclerView.adapter = RecyclerAdapter(list)
+
+        val appBar = view.findViewById<AppBarLayout>(R.id.app_bar)
+        val balanceNum = view.findViewById<TextView>(R.id.balance_num_app)
+        val cardVisa = view.findViewById<View>(R.id.card_visa)
+        val visaTxt = view.findViewById<TextView>(R.id.card_number)
+
+
+
+        // AppBar scroll animasiyası
+        appBar.addOnOffsetChangedListener { _, verticalOffset ->
+            val totalRange = appBar.totalScrollRange
+            val progress = -verticalOffset / totalRange.toFloat()
+
+            cardVisa.alpha = 1 - progress
+            visaTxt.alpha = 1 - progress
+            balanceNum.alpha = 1f
         }
+
+        cardVisa.transitionName = "card_transition" // Shared element adı
+
+        cardVisa.setOnClickListener {
+            // Flip animasiyası (y ekseni üzrə 180 dərəcə)
+            visaTxt.isInvisible
+
+            cardVisa.animate()
+                .rotationY(90f) // ön tərəfi gizlədir
+                .setDuration(200)
+                .withEndAction {
+                    // Flip tamamlanandan sonra yeni fragmentə keçid
+                    val extras = androidx.navigation.fragment.FragmentNavigatorExtras(
+                        cardVisa to "card_transition"
+                    )
+                    findNavController().navigate(
+                        R.id.action_nav_home_to_cardFragment,
+                        null,
+                        null,
+                        extras
+                    )
+                }
+                .start()
+        }
+
+
     }
 }
